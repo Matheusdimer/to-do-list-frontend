667725696616760db9c0297c72b45bc3bc4e97de
Matheusdimer <matheus-delima@outlook.com>
2021-01-19 10:21:49 -0300

    Ajustado formulário de registro

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -21,49 +21,50 @@ export default function Register() {
       {session.loggedIn && <Redirect to="/app" />}
       {isLoading && <Loading />}
       <div className="registerBox">
-        <p className="label">
-          Nome completo
-          {isFieldErr("name") && (
-            <span className="error-message">Não preenchido</span>
-          )}
-        </p>
-        <input
-          className={`auth-input ${isFieldErr("name") ? "error" : ""}`}
-          type="text"
-          onChange={({ target }) => setName(target.value)}
-        ></input>
-        <p className="label">
-          E-mail
-          {isFieldErr("email") && (
-            <span className="error-message">Usuário já existente</span>
-          )}
-        </p>
-        <input
-          className={`auth-input ${isFieldErr("email") ? "error" : ""}`}
-          type="text"
-          onChange={({ target }) => setEmail(target.value)}
-        ></input>
-        <p className="label">
-          Senha
-          {isFieldErr("password") && (
-            <span className="error-message">Não preenchido</span>
-          )}
-        </p>
-        <input
-          className="auth-input"
-          type="password"
-          onChange={({ target }) => setPassword(target.value)}
-        ></input>
-        <button
-          className="auth-button"
-          onClick={async () => {
+        <form
+          className="auth-form"
+          onSubmit={async (event) => {
+            event.preventDefault();
             setIsLoading(true);
             await signUp(email, name, password);
             setIsLoading(false);
           }}
         >
-          Criar conta
-        </button>
+          <p className="label">
+            Nome completo
+            {isFieldErr("name") && (
+              <span className="error-message">Não preenchido</span>
+            )}
+          </p>
+          <input
+            className={`auth-input ${isFieldErr("name") ? "error" : ""}`}
+            type="text"
+            onChange={({ target }) => setName(target.value)}
+          ></input>
+          <p className="label">
+            E-mail
+            {isFieldErr("email") && (
+              <span className="error-message">Usuário inválido</span>
+            )}
+          </p>
+          <input
+            className={`auth-input ${isFieldErr("email") ? "error" : ""}`}
+            type="text"
+            onChange={({ target }) => setEmail(target.value)}
+          ></input>
+          <p className="label">
+            Senha
+            {isFieldErr("password") && (
+              <span className="error-message">Não preenchido</span>
+            )}
+          </p>
+          <input
+            className="auth-input"
+            type="password"
+            onChange={({ target }) => setPassword(target.value)}
+          ></input>
+          <button className="auth-button">Criar conta</button>
+        </form>
       </div>
     </div>
   );
