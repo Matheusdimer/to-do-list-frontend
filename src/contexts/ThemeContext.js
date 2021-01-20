58b50c758ad9bb3db38495c4e35b8ce037676a43
Matheusdimer <matheus-delima@outlook.com>
2021-01-19 23:38:54 -0300

    Salvamento do tema no localStorage

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -5,14 +5,28 @@ const themes = require('../config/themes.json');
 export const ThemeContext = createContext()
 
 export function ThemeProvider({ children }) {
-  const [theme, setTheme] = useState(themes.light);
   const [dark, setDark] = useState(false);
+  const [theme, setTheme] = useState(() => {
+    let local_theme = localStorage.getItem('Theme')
+    local_theme = JSON.parse(local_theme);
+
+    if (local_theme){
+      if (local_theme.name === "Dark") {
+        setDark(true);
+      }
+      return local_theme;
+    } else {
+      return themes.light;
+    }
+  });
 
   function switchTheme(){
-    if (theme === themes.light) {
+    if (theme.name === "Light") {
       setTheme(themes.dark)
+      localStorage.setItem('Theme', JSON.stringify(themes.dark));
     } else {
       setTheme(themes.light)
+      localStorage.setItem('Theme', JSON.stringify(themes.light));
     }
 
     setDark(!dark)
